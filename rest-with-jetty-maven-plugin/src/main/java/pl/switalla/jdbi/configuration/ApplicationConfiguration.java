318e7ad9a7d70a9106d2c30dd8d267365a4dfdcc
Pawel Switalla <swit3k@gmail.com>
2015-09-13 11:23:32 +0200

    rest-with-jetty-maven-plugin added

---
+++ rest-with-jetty-maven-plugin/src/main/java/pl/switalla/jdbi/configuration/ApplicationConfiguration.java
@@ -0,0 +1,9 @@
+package pl.switalla.jdbi.configuration;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/resources")
+public class ApplicationConfiguration extends Application {
+    
+}
---
+++ rest-with-jetty-maven-plugin/src/main/java/pl/switalla/jdbi/resources/UserResource.java
@@ -0,0 +1,18 @@
+package pl.switalla.jdbi.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/user")
+public class UserResource {
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response user(@QueryParam("name") String name) {
+        return Response.ok(name + " ok test").build();
+    }
+}
